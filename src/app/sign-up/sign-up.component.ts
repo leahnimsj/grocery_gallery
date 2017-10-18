5eb89a3ccd803a676e6d7af3a2d776f6995462bb
Ruben <rubaco01@yahoo.com>
2017-10-18 10:10:56 -0700

    signup validation

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -12,9 +12,14 @@ import { DataService } from '../data.service';
 })
 export class SignUpComponent implements OnInit {
 
-  signUpForm: NgForm;
   successMessage: string;
   errorMessage: string;
+
+  signUpForm: NgForm;
+
+  @ViewChild('signUpForm') currentForm: NgForm;
+
+
   password;
   confirmPassword;
   passwordError;
@@ -47,18 +52,84 @@ export class SignUpComponent implements OnInit {
         this.passwordError = "Passwords don't match. Please try again."
 
 
-      }
- 
-      
-    
+      } 
 
   }
-
 
   clickButtonConsole(signUp: NgForm){
     console.log(signUp.value)
 
   }
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.signUpForm) { return; }
+    //set the form to the current form for comparison
+    this.signUpForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.signUpForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    let form = this.signUpForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+
+
+
+  formErrors = {
+    'firstName': '',
+    'lastName': '',
+    'email': '',
+    'password': '',
+    'confirmPassword': ''
+
+  };
+
+  validationMessages = {
+    'firstName': {
+      'required':  'First Name is a required field.',
+      'minlength': 'First Name cannot be less than 2 characters.',
+      'maxlength': 'First Name cannot be more than 255 characters.'
+    },
+    'lastName': {
+      'required':  'Last Name is a required field.',
+      'minlength': 'Last Name cannot be less than 2 characters.',
+      'maxlength': 'Last Name cannot be more than 255 characters.'
+    },
+    'email': {
+      'required':  'Email is a required field.',
+      'pattern': 'Email must be a valid emai format'
+    },
+
+    'password': {
+      'required':  'Password is a required field.',
+      'minlength': 'Password cannot be less than 6 characters.'
+    },
+
+    'confirmPassword': {
+      'required':  'Confirm Password is a required field.',
+      'minlength': 'Confirm Passsword cannot be less than 6 characters.'
+    }
+  };
 
 
 
