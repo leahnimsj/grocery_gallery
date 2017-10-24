abbe4e118650d05e6bc77a0e596f5b71d3d2533f
Ruben <rubaco01@yahoo.com>
2017-10-24 09:04:06 -0700

    fridge_continued_styling

--- src/app/grocery-email-form/grocery-email-form.component.ts
+++ src/app/grocery-email-form/grocery-email-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject, Input, Output } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatIconRegistry } from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-grocery-email-form',
@@ -12,7 +13,15 @@ export class GroceryEmailFormComponent implements OnInit {
 
   sharedEmail = 'go@gmail.com'; 
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public iconRegistry: MatIconRegistry, 
+    private sanitizer: DomSanitizer,
+) 
+
+  { 
+    iconRegistry.addSvgIcon('cross-out', sanitizer.bypassSecurityTrustResourceUrl('assets/images/cross-out.svg'))
+  }
 
   ngOnInit() {
 
